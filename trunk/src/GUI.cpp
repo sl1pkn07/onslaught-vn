f569c2be6edea1e34aa35324b2e35c5382b257a3
helios_vmg <helios_vmg@0b986520-c2e1-4ba6-b813-47c2024ae449>
2011-03-12 02:06:26 +0000

    * Fixing improper calls to NONS_EventQueue::emptify() that were causing undue exit when leaving the debugging console. * Fixing animation bug. * Improving debugging console autocomplete algorithm.

--- trunk/src/GUI.cpp
+++ trunk/src/GUI.cpp
@@ -776,7 +776,7 @@ int NONS_ButtonLayer::getUserInput(int x,int y,bool override_placement){
 					case SDLK_PAUSE:
 						if (console){
 							console->enter(this->screen);
-							if (!queue.emptify())
+							if (queue.emptify())
 								return INT_MIN;
 						}
 						break;
@@ -953,7 +953,7 @@ int NONS_ButtonLayer::getUserInput(ulong expiration){
 							case SDLK_PAUSE:
 								if (console){
 									console->enter(this->screen);
-									if (!queue.emptify())
+									if (queue.emptify())
 										return INT_MIN;
 								}
 								break;
@@ -1878,7 +1878,7 @@ void NONS_DebuggingConsole::autocomplete(std::vector<std::wstring> &dst,const st
 	std::wstring first,
 		second,
 		third=line.substr(cursor);
-	ulong cutoff=line.find_last_of(WCS_WHITESPACE,cursor);
+	ulong cutoff=find_last_not_of_id(line,cursor);
 	if (cutoff==line.npos)
 		cutoff=0;
 	else
