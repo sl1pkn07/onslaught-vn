3a9d9053b1338f5bc5689916920cb429f44cde37
Víctor M. González <helios.vmg@gmail.com>
2012-07-23 07:11:54 +0000

    * Fixing minor bug in MIDI decoder initializer. * Fixing minor compiler warnings. * Fixing minor macro-related bug(?). * Windows: giving high priority threads system-wide high priority. * Fixing compilation errors with Clang under Windows.

--- src/AudioFormats.cpp
+++ src/AudioFormats.cpp
@@ -389,12 +389,15 @@ BOOL openal_IsPresent(){
 	return 1;
 }
 
+static char drv_openal_str1[]="openal",
+	drv_openal_str2[]="OpenAL";
+
 MDRIVER drv_openal={
 	NULL,
-	"openal",
-	"OpenAL",
+	drv_openal_str1,
+	drv_openal_str2,
 	0,255,
-	"openal",
+	drv_openal_str1,
 
 	0,
 	openal_IsPresent,
@@ -539,10 +542,10 @@ custom_stdio init_stdio(){
 
 bool midi_static_data::perform_initialization(){
 	custom_stdio stdio=init_stdio();
-	if (mid_init(0,&stdio))
-		return 0;
 	if (!general_archive.addArchive(L"timidity.zip"))
 		general_archive.addArchive(L"timidity.oaf");
+	if (mid_init(0,&stdio))
+		return 0;
 	return 1;
 }
 
